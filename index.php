95c108c91c96f1519ebf6f5e161f8a4a6d37f9c6
Fabrizio <fabriziospoto1@gmail.com>
2020-09-17 17:41:40 +0200

    Completo 2.0
    Completo 2.0

--- index.php
+++ index.php
@@ -77,13 +77,16 @@ $faq = [
         </main>
         <footer class="clearfix">
             <div class="container">
-                <div>
+                <div class="terms clearfix">
                     <p><a href="#"></a>Google</p>
                     <p><a href="#"></a>Tutto su Google</p>
                     <p><a href="#"></a>Privacy</p>
                     <p><a href="#"></a>Termini</p>
                 </div>
-                <div class="language">
+                <div class="language clearfix">
+                    <div class="lang-logo">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAMAAAAMs7fIAAAABGdBTUEAALGPC/xhBQAAAFpQTFRFAAAAc8b%2BeNj%2BeLL7lt3/YbL4cs/9rNn8p9b7%2Bfn5hdD/a8D/7vD/vOL9itf%2B////3PD%2Byuj9R6348Pb7KZz2UL78MKL3Oqv4Zsz/ndX8qN/%2BI5T0S7X4W8b9mmuivQAAAAF0Uk5TAEDm2GYAAACXSURBVBjTXYqLEsIwCARR%2B27zpCFNxP//TSGOTu3OhDs2wGaPM9bD8hKGMDtnDq130GkxDK/BY/tVc6CtXa21Q72ClFKoqUrINBLAzHP9grKpccxFYmqNIcY4j%2BXDiLJBztmUXCRkGgkgIsZbO1kwEjVDKwYmDriSGt6FPHrnp6x1Bd/vzxP9Bj8mFfBH2K8Gtng1AA95bxQhE7o%2B68UQAAAAAElFTkSuQmCC" alt="Language">
+                    </div>
                     <select class="select">
                         <option value="Italiano">Italiano</option>
                         <option value="English">English</option>
