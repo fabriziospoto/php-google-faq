2a4991590a9eec45fab8ca065134f67d4d4a0ad7
Fabrizio <fabriziospoto1@gmail.com>
2020-09-17 15:44:45 +0200

    Init
    Init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <title>php-google-faq</title>
+    </head>
+    <body>
+
+    </body>
+</html>
